090dd5156902f4ee8c080400adad0dfef5711ca4
Javkonline <95667371+Javkonline@users.noreply.github.com>
2025-09-13 21:37:32 +0800

    Update run_bash_accelerate.sh

--- run_amopo/run_bash_accelerate.sh
+++ run_amopo/run_bash_accelerate.sh
@@ -1,12 +1,9 @@
-set -x
-export CUDA_DEVICE_ORDER="PCI_BUS_ID"
-export PATH="/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtai/users/liuqi138/env/llama_fact/bin:$PATH" 
+
 
 
 NNODES=1
 GPUS_PER_NODE=2
 WORLD_SIZE=$(($NNODES*$GPUS_PER_NODE))
-GRAD_ACC_STEPS=1
 echo NNODES:${NNODES}
 echo GPUS_PER_NODE:${GPUS_PER_NODE}
 echo WORLD_SIZE:${WORLD_SIZE}
@@ -16,40 +13,24 @@ GRADIENT_ACCUMULATION_STEPS=1
 PRE_DEVICE_TRAIN_BATCH_SIZE=1
 TRAIN_BATCH_SIZE=$(($NNODES*$GPUS_PER_NODE*$GRADIENT_ACCUMULATION_STEPS*$PRE_DEVICE_TRAIN_BATCH_SIZE))
 FLAG="uniform_sample"
-# # dpo
-# PREF_BETA=0.01
-# LEARNING_RATE=5e-7
-# PREF_LOSS=sigmoid
-# simpo
 SIMPO_GAMMA=2.0
 PREF_BETA=0.8
 LEARNING_RATE=3e-7
-PREF_LOSS=simpo
 NUM_EPOCH=3.0
 
-# # qwen2.5-14B-ins
-# OUTPUT_DIR=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtai/users/liuqi138/instruction_follow/saves/qwen2.5-14B-ins/full_deepspeed/rmopo/mo_PREF_LOSS_${PREF_LOSS}_SIMPO_GAMMA_${SIMPO_GAMMA}_PREF_BETA_${PREF_BETA}_LEARNING_RATE_${LEARNING_RATE}_TRAIN_BATCH_SIZE_${TRAIN_BATCH_SIZE}_FLAG_${FLAG}_NUM_EPOCH_${NUM_EPOCH}_/
-
-# # qwen2.5-7B-ins
-# OUTPUT_DIR=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtai/users/liuqi138/instruction_follow/saves/qwen2.5-7B-ins/full_deepspeed/rmopo/mo_PREF_LOSS_${PREF_LOSS}_SIMPO_GAMMA_${SIMPO_GAMMA}_PREF_BETA_${PREF_BETA}_LEARNING_RATE_${LEARNING_RATE}_TRAIN_BATCH_SIZE_${TRAIN_BATCH_SIZE}_FLAG_${FLAG}_NUM_EPOCH_${NUM_EPOCH}_/
-
-# # qwen2.5-32B-ins
-# OUTPUT_DIR=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtai/users/liuqi138/instruction_follow/saves/qwen2.5-32B-ins/full_deepspeed/rmopo/mo_PREF_LOSS_${PREF_LOSS}_SIMPO_GAMMA_${SIMPO_GAMMA}_PREF_BETA_${PREF_BETA}_LEARNING_RATE_${LEARNING_RATE}_TRAIN_BATCH_SIZE_${TRAIN_BATCH_SIZE}_FLAG_${FLAG}_NUM_EPOCH_${NUM_EPOCH}_/
-
 # qwen2.5-32B-ins
-OUTPUT_DIR=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtai/users/liuqi138/instruction_follow/saves/qwen2.5-0.5B-ins/full_deepspeed/rmopo/mo_PREF_LOSS_${PREF_LOSS}_SIMPO_GAMMA_${SIMPO_GAMMA}_PREF_BETA_${PREF_BETA}_LEARNING_RATE_${LEARNING_RATE}_TRAIN_BATCH_SIZE_${TRAIN_BATCH_SIZE}_FLAG_${FLAG}_NUM_EPOCH_${NUM_EPOCH}_/
-# # llama3.1-8B-ins
-# OUTPUT_DIR=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtai/users/liuqi138/instruction_follow/saves/llama3.1-8b-ins/full_deepspeed/rmopo/mo_PREF_LOSS_${PREF_LOSS}_SIMPO_GAMMA_${SIMPO_GAMMA}_PREF_BETA_${PREF_BETA}_LEARNING_RATE_${LEARNING_RATE}_TRAIN_BATCH_SIZE_${TRAIN_BATCH_SIZE}_FLAG_${FLAG}_NUM_EPOCH_${NUM_EPOCH}_/
+Model_PATH="Qwen2.5-0.5B-Instruct"
+OUTPUT_DIR=/OUTPUT/AMOPO
 echo OUTPUT_DIR:$OUTPUT_DIR
 
 accelerate launch \
---config_file /mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtai/users/liuqi138/LLaMA-Factory/run_amopo/deepspeed_config.yaml \
---gradient_accumulation_steps $GRAD_ACC_STEPS \
+--config_file run_amopo/deepspeed_config.yaml \
+--gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS \
 --num_machines $NNODES \
 --num_processes $WORLD_SIZE \
 src/train.py \
 --stage amopo \
---model_name_or_path /mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtai/users/liuqi138/llm/Qwen2.5-0.5B-Instruct \
+--model_name_or_path $Model_PATH \
 --do_train \
 --dataset help_steer2_train_format_po2 \
 --template qwen \
@@ -57,7 +38,6 @@ src/train.py \
 --lora_target  all \
 --pref_beta  $PREF_BETA \
 --simpo_gamma $SIMPO_GAMMA \
---pref_loss $PREF_LOSS \
 --cutoff_len 1024 \
 --max_samples  10000 \
 --preprocessing_num_workers  16 \
